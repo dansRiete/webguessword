19c0c0cb300f8711cd3b90ecb11a084cff48a3fe
dansRiete <vlenaf13>
2016-05-05 22:14:32 +0300

    gsg

--- src/main/java/beans/FirstBean.java
+++ src/main/java/beans/FirstBean.java
@@ -1,16 +1,10 @@
 package beans;
 
 import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Random;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
 
 /**
  * Created by Aleks on 23.04.2016.
@@ -21,7 +15,8 @@ import javax.sql.DataSource;
 public class FirstBean implements Serializable{
     private String name = "Default name1";
     private String text = "Default text";
-    static String host3 = "jdbc:mysql://127.3.47.130:3306/guessword?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static String host1 = "jdbc:mysql://127.3.47.130:3306/guessword?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static String host2 = "jdbc:mysql://127.0.0.1:3307/guessword?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     Random random = new Random();
 
     public FirstBean(){}
@@ -46,13 +41,21 @@ public class FirstBean implements Serializable{
         text = new Integer(random.nextInt(999999)).toString();
     }
 
-    public String returnFromBase() throws Exception{
+    public String returnFromBase() throws SQLException{
 //        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(host3, "adminLtuHq9R", "d-AUIKakd1Br");
-        Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM aleks");
-        rs.next();
-        return rs.getString("engname")+" - " + rs.getString("rusname");
+        try{
+            Connection conn = DriverManager.getConnection(host1, "adminLtuHq9R", "d-AUIKakd1Br");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM aleks");
+            rs.next();
+            return rs.getString("engname")+" - " + rs.getString("rusname");
+        }catch (SQLException e){
+            Connection conn = DriverManager.getConnection(host2, "adminLtuHq9R", "d-AUIKakd1Br");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM aleks");
+            rs.next();
+            return rs.getString("engname")+" - " + rs.getString("rusname");
+        }
 
     }
 
