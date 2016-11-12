6aff6e92bdc594dd5a23fa897be0850538f88a0d
dansRiete <vlenaf13>
2016-11-12 14:01:40 +0200

    ...

--- src/main/java/logic/Answer.java
+++ src/main/java/logic/Answer.java
@@ -9,84 +9,112 @@ import java.util.regex.PatternSyntaxException;
  */
 public class Answer {
     private boolean answerIsCorrect;
-    private final String[] givenAnswers;
-    private final String[] referenceAnswers;
+    private final String[] givenPhrases;
+    private final String[] referencePhrases;
+    private final static int SYLLABLE_SIZE = 2;
 
     public Answer(String answer, Phrase question) {
-        this.givenAnswers = answer.split("/");
-        this.referenceAnswers = question.getForeignWord().split("/");
+        this.givenPhrases = answer.split("[/\\\\]");
+        this.referencePhrases = question.getForeignWord().split("[/\\\\]");
     }
 
-    private void checkTheAnswer(){
+    private String removeDoubleLetters(String givenWord){
+        StringBuilder shortAnswer = new StringBuilder().append(givenWord.charAt(0));
+        for (int i = 1, y = 0; i < givenWord.length(); i++) {
+            if (givenWord.toLowerCase().charAt(i) != shortAnswer.charAt(y)) {
+                shortAnswer.append(givenWord.toLowerCase().charAt(i));
+                y++;
+            }
+        }
+        return shortAnswer.toString();
+    }
+
+    private String[] divideOntoSyllables(String givenPhrase){
+        int syllabelsSize = 2;
+        int positionInWord = 0;
+        int wordsPosition = 0;
+        String[] words = givenPhrase.split(" ");
+        String[] syllables = new String[givenPhrase.length() / syllabelsSize];
+        for(String currentWord : words){
+            if(currentWord.length() <= 3){
+                words[wordsPosition] = currentWord;
+                wordsPosition++;
+            }else {
+
+            }
+        }
+
 
     }
 
-    public void checkTheAnswer2(String firstWord, String secondWord, boolean exactMatch) {
-
-        if(firstWord.equalsIgnoreCase(secondWord)){
-            answerIsCorrect = true;
-        } else if(!exactMatch){
-
-            String shortedWord = String.valueOf(firstWord.charAt(0));
-            String shortAnswer;
-            int numOfSyllables;
-            for (int i = 1, y = 0; i < firstWord.length(); i++) {
-                // убирает двойные буквы из правильного ответа
-                if (firstWord.toLowerCase().charAt(i) != shortedWord.charAt(y)){
-                    shortedWord += firstWord.toLowerCase().charAt(i);
-                    y++;
+    private boolean checkPhrase(String givenPhrase, String referencePhrase){
+        String shortedGivenPhrase = removeDoubleLetters(givenPhrase);
+        String shortedReferencePhrase = removeDoubleLetters(referencePhrase);
+        String[] referenceWords = shortedReferencePhrase.split(" ");
+        String[] givenWords = shortedGivenPhrase.split(" ");
+        if(referenceWords.length != givenWords.length){
+            return false;
+        }else {
+            for (int i = 0; i < givenWords.length; i++){
+                if(!givenWords[i].equalsIgnoreCase(referenceWords[i])){
+                    return false;
                 }
             }
-            if(secondWord.length()==0){
-                shortAnswer=secondWord;
-            }
-            else {
-                shortAnswer = String.valueOf(secondWord.charAt(0));
-                for (int i = 1, y = 0; i < secondWord.length(); i++) {
-                    // убирает двойные буквы из ответа
-                    if (secondWord.toLowerCase().charAt(i) != shortAnswer.charAt(y)) {
-                        shortAnswer += secondWord.toLowerCase().charAt(i);
-                        y++;
-                    }
-                }
-            }
-            if (shortedWord.equalsIgnoreCase(secondWord)){
-                //            System.out.println("Сработал Интелифайнд");   //Сигнализирует о том что Интелифайнд вернул true
+        }
+    }
+
+    private boolean equalsBySyllables(String givenWord, String referenceWord){
+        if(referenceWord.length() <= 5 && !givenWord.equalsIgnoreCase(referenceWord)){
+            return false;
+        }else {
+
+        }
+    }
+
+    private String[] divideOntoEvenSyllables(String givenWord){
+
+    }
+
+    private boolean checkOnMatch(String givenWord, String referenceWord) {
+
+        if(givenWord.equalsIgnoreCase(referenceWord)){
+            return true;
+        } else {
+            int syllablesAmount;
+            String shortedTestableWord = removeDoubleLetters(givenWord);
+            if (shortedTestableWord.equalsIgnoreCase(referenceWord)){
                 answerIsCorrect = true;
             }
-            else if (shortedWord.length() < 5)
+            else if (shortedTestableWord.length() < 5)
                 answerIsCorrect = false;
             else {
                 //разбиваем shortWord на слоги
-                numOfSyllables = shortedWord.length() / 2; //вычисляем количество слогов по3
-                if (shortedWord.length() % 2 > 0)
-                    numOfSyllables = numOfSyllables + 1;
+                syllablesAmount = shortedTestableWord.length() / 2; //вычисляем количество слогов по3
+                if (shortedTestableWord.length() % 2 > 0)
+                    syllablesAmount = syllablesAmount + 1;
                 //System.out.println("Количество слогов в shortWord=" + b);
 
-                String arWord[] = new String[numOfSyllables];
-                for (int i = 0; i < numOfSyllables; i++) {
+                String arWord[] = new String[syllablesAmount];
+                for (int i = 0; i < syllablesAmount; i++) {
                     //Заполняем массив arWord[] слогами
-                    arWord[i] = shortedWord.substring(i*2, (i*2+2 > shortedWord.length()) ? shortedWord.length() : i*2+2);
+                    arWord[i] = shortedTestableWord.substring(i*2, (i*2+2 > shortedTestableWord.length()) ? shortedTestableWord.length() : i*2+2);
                 }
                 float matchedSyllables = 0;
-                for (int i = 0; i < arWord.length; i++){
-                    try{
-                        Pattern pat = Pattern.compile(arWord[i]);
-                        Matcher match = pat.matcher(shortAnswer);
-                        if (match.find())
+                for (String anArWord : arWord) {
+                    try {
+                        Matcher match = Pattern.compile(anArWord).matcher(shortedTestableWord);
+                        if (match.find()) {
                             matchedSyllables++;
-                    }
-                    catch (PatternSyntaxException e){
+                        }
+                    } catch (PatternSyntaxException e) {
+                        e.printStackTrace();
                     }
                 }
-                float f=matchedSyllables/(float)numOfSyllables;
-                //            if(f>0.7)
-                //                System.out.println("Сработал Интелифайнд");   //Сигнализирует о том что Интелифайнд вернул true
+                float f=matchedSyllables/(float)syllablesAmount;
                 answerIsCorrect = f>0.7;
-
             }
-        }else
-            answerIsCorrect = false;
+        }
+        return false;
 
     }
 
---
+++ src/test/java/TestAnswer.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+public class TestAnswer {
+
+    @org.junit.Test
+    public void randomDividingTest() {
+
+        assertEquals(5.0, 5.0, 0);
+    }
+}
